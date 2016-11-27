6c296c345a5bb5d3738ea71b9c8909858da3a3c8
minh141198 <phanducnhatminh@gmail.com>
2016-11-27 19:16:20 +0700

    Update Biconcomps

---
+++ BiConComps.cpp
@@ -0,0 +1,40 @@
+const int N = 1024;
+
+int count, parent[N], n; //n vertices 0..n-1
+bool visited[N];
+vector<int> G[N];
+stack<pair<int, int> > s;
+
+void OutputComp(int u, int v) {
+	pair<int, int> edge;
+	do {
+		edge = s.top(); s.pop();
+		printf("%d %d\n", edge.first, edge.second);
+	} while (edge != make_pair(u, v));
+	printf("\n");
+}
+
+void dfs(int u) {
+	visited[u] = true;
+	count++;
+	low[u] = num[u] = count;
+	for (int v : G[u]) {
+		if (!visited[v]) {
+			s.push({u, v});
+			parent[v] = u;
+			dfs(v);
+			if (low[v] > num[u]) OutputComp(u, v);
+			low[u] = min(low[u], low[v]);
+		} else if (parent[u] != v && num[v] < num[u]) {
+			s.push({u, v});
+			low[u] = min(low[u], num[v]);
+		}
+	}
+}
+
+void BiconnectedComponents {
+	count = 0;
+	memset(parent, -1, sizeof parent);
+	for (int i = 0; i < n; i++)
+		if (!visited[i]) dfs(i);
+}
