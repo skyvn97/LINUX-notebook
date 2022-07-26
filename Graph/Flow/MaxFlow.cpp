d666649eafcaad5d97b08dedb1351678ceb9262a
Hanh Van Pham <skyvn97@yahoo.com>
2022-07-26 09:49:56 +0700

    Update Dinitz Flow

--- Graph/Flow/MaxFlow.cpp
+++ Graph/Flow/MaxFlow.cpp
@@ -1,63 +1,69 @@
 struct DinicFlow {
-    static const int INF=(int)1e9+7;
-    vector<int> dist,head,q,work;
-    vector<int> point,capa,flow,next;
-    int n,m;
-
-    DinicFlow() {
-        n=0;m=0;
+    static const long long INF = (long long) 1e18 + 7;
+    int numNode, numEdge;
+    vector<int> dist, head, work;
+    vector<int> point, next;
+    vector<long long> flow, capa;
+ 
+    DinicFlow(int n = 0) {
+        numNode = n;
+        numEdge = 0;
+        dist.assign(n + 7, 0);
+        head.assign(n + 7, -1);
+        work.assign(n + 7, 0);
     }
-    DinicFlow(int n) {
-        this->n=n;m=0;
-        dist.assign(n+7,0);
-        head.assign(n+7,-1);
-        q.assign(n+7,0);
-        work.assign(n+7,0);
+ 
+    int addEdge(int u, int v, long long c1, long long c2 = 0) {
+        int ret = numEdge;
+        point.push_back(v); capa.push_back(c1); flow.push_back(0); next.push_back(head[u]); head[u] = numEdge++;
+        point.push_back(u); capa.push_back(c2); flow.push_back(0); next.push_back(head[v]); head[v] = numEdge++;
+        return ret;
     }
-    void addEdge(int u,int v,int c1,int c2) {
-        point.push_back(v);capa.push_back(c1);flow.push_back(0);next.push_back(head[u]);head[u]=m++;
-        point.push_back(u);capa.push_back(c2);flow.push_back(0);next.push_back(head[v]);head[v]=m++;
-    }
-    bool bfs(int s,int t) {
-        fill(dist.begin(), dist.end(), -1);
-        int sz=0;
-        q[sz++]=s;dist[s]=0;
-        for (int x=0;x<sz;x=x+1) {
-            int u=q[x];
-            for (int i=head[u];i>=0;i=next[i])
-                if (dist[point[i]]<0 && flow[i]<capa[i]) {
-                    dist[point[i]]=dist[u]+1;
-                    q[sz++]=point[i];
-                }
-        }
-        return (dist[t]>=0);
-    }
-    int dfs(int s,int t,int f) {
-        if (s==t) return (f);
-        for (int &i=work[s];i>=0;i=next[i])
-            if (dist[point[i]]==dist[s]+1 && flow[i]<capa[i]) {
-                int d=dfs(point[i],t,min(f,capa[i]-flow[i]));
-                if (d>0) {
-                    flow[i]+=d;
-                    flow[i^1]-=d;
-                    return (d);
-                }
+ 
+    bool bfs(int s, int t) {
+        queue<int> q;
+        for (int i = 1; i <= numNode; i++) dist[i] = -1;
+        dist[s] = 0; q.push(s);
+        while (!q.empty()) {
+            int u = q.front(); q.pop();
+            for (int i = head[u]; i >= 0; i = next[i]) if (flow[i] < capa[i] && dist[point[i]] < 0) {
+                dist[point[i]] = dist[u] + 1;
+                q.push(point[i]);
             }
-        return (0);
+        }
+        return dist[t] >= 0;
     }
-    int maxFlow(int s,int t) {
-        int totflow=0;
-        while (bfs(s,t)) {
-            copy(head.begin(), head.end(), work.begin());
+    long long dfs(int s, int t, long long f) {
+        if (s == t) return f;
+        for (int &i = work[s]; i >= 0; i = next[i]) if (flow[i] < capa[i] && dist[point[i]] == dist[s] + 1) {
+            long long d = dfs(point[i], t, min(f, capa[i] - flow[i]));
+            if (d > 0) {
+                flow[i] += d;
+                flow[i ^ 1] -= d;
+                return d;
+            }
+        }
+        return 0;
+    }
+ 
+    long long maxFlow(int s, int t) {
+        long long totFlow = 0;
+        while (bfs(s, t)) {
+            for (int i = 1; i <= numNode; i++) work[i] = head[i];
             while (true) {
-                int d=dfs(s,t,INF);
-                if (d<=0) break;
-                totflow+=d;
+                int d = dfs(s, t, INF);
+                if (d == 0) break;
+                totFlow += d;
             }
         }
-        return (totflow);
+        return totFlow;
     }
-    void resetFlow() {
-        fill(flow.begin(), flow.end(), 0);
+ 
+    int getFlow(int id) const {
+        return flow[id];
+    }
+ 
+    bool visited(int node) const {
+        return dist[node] >= 0;
     }
 };
