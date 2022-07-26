8076354c8da5459b66cacf5daaa595a71df523a3
Hanh Van Pham <skyvn97@yahoo.com>
2022-07-26 16:25:45 +0700

    Update MaxFlowMinCost.cpp

--- Graph/Flow/MaxFlowMinCost.cpp
+++ Graph/Flow/MaxFlowMinCost.cpp
@@ -1,75 +1,91 @@
-#define FORE(i,v) for (__typeof((v).begin()) i=(v).begin();i!=(v).end();i++)
-    
 class MaxFlowMinCost {
-    private:
-    static const int INF=(int)1e9+7;
+    static const int INF = (int)1e9 + 7;
+    static const long long LL_INF = (long long)1e18 + 7LL;
+ 
     struct Edge {
-        int from,to,capa,flow,cost;
-        Edge() {
-            from=to=capa=flow=cost=0;
+        int from, to, flow, capa;
+        long long cost;
+ 
+        Edge(int _from = 0, int _to = 0, int _capa = 0, long long _cost = 0) {
+            from = _from; to = _to; flow = 0; capa = _capa; cost = _cost;
         }
-        Edge(int u,int v,int ca,int co) {
-            from=u;to=v;capa=ca;flow=0;cost=co;
-        }
-        int residental(void) const {
-            return (capa-flow);
+ 
+        int residual(void) const {
+            return capa - flow;
         }
     };
-    int n;
-    vector<vector<int> > adj;
-    vector<Edge> edge;
-    vector<int> dist,trace;
-    bool FordBellman(int s,int t) {
-        fill(dist.begin(), dist.end(), INF);
-        fill(trace.begin(), trace.end(), -1);
-        vector<bool> inQueue(n+7,false);
+ 
+    int numNode;
+    vector<vector<int>> adj;
+    vector<Edge> edges;
+    vector<long long> dist;
+    vector<int> trc;
+    vector<bool> inQueue;
+ 
+public:
+    MaxFlowMinCost(int _numNode = 0) {
+        numNode = _numNode;
+        adj.assign(numNode + 7, vector<int>());
+        dist.assign(numNode + 7, 0);
+        trc.assign(numNode + 7, -1);
+        inQueue.assign(numNode + 7, false);
+    }
+ 
+    int addEdge(int from, int to, int capa, long long cost) {
+        adj[from].push_back(edges.size());
+        edges.push_back(Edge(from, to, capa, cost));
+        adj[to].push_back(edges.size());
+        edges.push_back(Edge(to, from, 0, -cost));
+        return edges.size() - 2;
+    }
+ 
+private:
+    bool fordBellman(int s, int t) {
+        FOR(i, 1, numNode) {
+            dist[i] = LL_INF;
+            trc[i] = -1;
+            inQueue[i] = false;
+        }
+ 
         queue<int> q;
-        dist[s]=0;q.push(s);inQueue[s]=true;
+        dist[s] = 0; q.push(s); inQueue[s] = true;
+ 
         while (!q.empty()) {
-            int u=q.front();q.pop();inQueue[u]=false;
-            FORE(it,adj[u]) if (edge[*it].residental()>0) {
-                int v=edge[*it].to;
-                if (dist[v]>dist[u]+edge[*it].cost) {
-                    dist[v]=dist[u]+edge[*it].cost;
-                    trace[v]=*it;
+            int u = q.front(); q.pop(); inQueue[u] = false;
+            for (int id : adj[u]) if (edges[id].residual() > 0) {
+                int v = edges[id].to;
+                if (minimize(dist[v], dist[u] + edges[id].cost)) {
+                    trc[v] = id;
                     if (!inQueue[v]) {
-                        q.push(v);inQueue[v]=true;
+                        q.push(v); inQueue[v] = true;
                     }
                 }
             }
         }
-        return (dist[t]<INF);
+ 
+        return dist[t] < LL_INF;
     }
-    public:
-    MaxFlowMinCost() {
-        n=0;
-    }
-    MaxFlowMinCost(int n) {
-        this->n=n;
-        adj.assign(n+7,vector<int>());
-        dist.assign(n+7,0);
-        trace.assign(n+7,0);
-    }
-    void addEdge(int u,int v,int ca,int co) {
-        adj[u].push_back(edge.size());
-        edge.push_back(Edge(u,v,ca,co));
-        adj[v].push_back(edge.size());
-        edge.push_back(Edge(v,u,0,-co));
-    }
-    pair<int,int> getFlow(int s,int t) {
-        int totFlow=0;
-        int totCost=0;
-        while (FordBellman(s,t)) {
-            int delta=INF;
-            for (int u=t;u!=s;u=edge[trace[u]].from)
-                delta=min(delta,edge[trace[u]].residental());
-            for (int u=t;u!=s;u=edge[trace[u]].from) {
-                edge[trace[u]].flow+=delta;
-                edge[trace[u]^1].flow-=delta;
+ 
+public:
+    pair<int, long long> getFlow(int src, int snk) {
+        int totFlow = 0;
+        long long totCost = 0;
+ 
+        while (fordBellman(src, snk)) {
+            int delta = INF;
+            for (int u = snk; u != src; u = edges[trc[u]].from) minimize(delta, edges[trc[u]].residual());
+            for (int u = snk; u != src; u = edges[trc[u]].from) {
+                edges[trc[u]].flow += delta;
+                edges[trc[u] ^ 1].flow -= delta;
             }
-            totFlow+=delta;
-            totCost+=delta*dist[t];
+            totFlow += delta;
+            totCost += delta * dist[snk];
         }
-        return (make_pair(totFlow,totCost));
+ 
+        return make_pair(totFlow, totCost);
+    }
+ 
+    int traceFlow(int id) const {
+        return edges[id].flow;
     }
 };
